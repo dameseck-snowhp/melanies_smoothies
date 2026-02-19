323042bba6420e726e73052aacb97eae7fc6ef04
dameseck-snowhp <dame.seck@smile.fr>
2026-02-19 15:28:53 +0100

    Update streamlit_app.py
    add pandas for test

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,11 @@ name_on_order = st.text_input("Name on smoothie:")
 st.write("The name on your Smoothie will be: ", name_on_order)
 cnn = st.connection("snowflake")
 session = cnn.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
