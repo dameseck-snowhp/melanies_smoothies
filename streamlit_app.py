ddeeb36fc184de089a66333471aa9bf69216ce48
dameseck-snowhp <dame.seck@smile.fr>
2026-02-19 10:26:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -41,3 +42,5 @@ if ingredients_list :
     #if ingredients_string:
      #   session.sql(my_insert_stmt).collect()
       #  st.success('Your Smoothie is ordered,{}!'.format(name_on_order), icon="✅")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
